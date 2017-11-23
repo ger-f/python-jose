67a6479ad2cc41c5705bb67d9fa22b3b72ea62c3
Ger Fitzpatrick <ger@eaglepoint.partners>
2017-11-23 13:54:57 +0100

    Fixed python3 bug

--- jose/jws.py
+++ jose/jws.py
@@ -219,7 +219,8 @@ def _sig_matches_keys(keys, signing_input, signature, alg):
 def _get_keys(key):
 
     try:
-        key = json.loads(key)
+        key = json.loads(key.decode('utf-8'))
+        # key = json.loads(key)
     except Exception:
         pass
 
